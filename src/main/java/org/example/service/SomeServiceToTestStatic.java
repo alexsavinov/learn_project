b8b1bd62c991827fde77aab0e5bee48dc77b9194
Viktor_Gritsaenko <viktor_gritsaenko@epam.com>
2023-03-09 09:34:30 +0200

    update SomeServiceToTestStatic

--- src/main/java/org/example/service/SomeServiceToTestStatic.java
+++ src/main/java/org/example/service/SomeServiceToTestStatic.java
@@ -6,8 +6,13 @@ import java.time.LocalDateTime;
 
 public class SomeServiceToTestStatic {
 
-    // TODO: how would you mock static? Try writing a unit test for it
-    public LocalDateTime someMethodToTestStatic(Long id) {
+    // TODO: you can mock someMethodToTestStatic() using Spy to test this method.
+    public LocalDateTime someMethodToTestSpy() {
+        return someMethodToTestStatic();
+    }
+
+    // TODO: how would you mock static? Try writing a unit test for it.
+    public LocalDateTime someMethodToTestStatic() {
         return DateUtil.now();
     }
 }
